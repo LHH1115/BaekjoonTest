cdf5f690924d0416af5e20782551827f41808e4c
이호형 <77003178+LHH1115@users.noreply.github.com>
2023-05-25 14:49:18 +0900

    Update 소트인사이드.java

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -6,19 +6,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String N = sc.next();
-        int[] arr = new int[N.length()];
-        for(int i=0; i<N.length(); i++){
-            arr[i] = Integer.parseInt(N.substring(i, i+1));
-        }
+        char[] arr = sc.nextLine().toCharArray();
 
         Arrays.sort(arr);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=arr.length-1; i>=0; i--){
-            sb.append(arr[i]);
-        }
-
-        System.out.println(sb.toString());
+        for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
     }
 }
